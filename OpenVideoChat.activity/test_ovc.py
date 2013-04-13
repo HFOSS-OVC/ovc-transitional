ed66ac07a4480058c6be736370c2081e16792bf7
CCoffie <CalebCoffie@gmail.com>
2013-04-13 11:01:02 -0400

    Created debug script

---
+++ OpenVideoChat.activity/test_ovc.py
@@ -0,0 +1,24 @@
+#	Created by Caleb Coffie
+#	Used to expendite the process of testing and
+#	and debugging on the XO
+
+import os
+
+os.system("git pull")
+
+os.system("sudo python setup.py build")
+os.system("sudo python setup.py dist_xo")
+
+os.system("sugar-install-bundle OpenVideoChat-1.xo")
+
+os.system("echo \"---------------\" >> ErrorLog.txt")
+os.system("date >> ErrorLog.txt")
+os.system("echo \"---------------\" >> ErrorLog.txt")
+
+
+os.system("sugar-launch org.laptop.OpenVideoChat &>> ErrorLog.txt")
+
+os.system("git add ErrorLog.txt")
+os.system("git commit -m\"Updating Error Log.\"")
+
+os.system("git push")
